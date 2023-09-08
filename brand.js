dbc952e14a879fd9a41b2f4f484c4c534bcdb04d
SUMON <belalhossain22000@gmail.comgit config --global user.name>
2023-09-09 00:53:04 +0600

    modify add some section

---
+++ brand.js
@@ -0,0 +1,29 @@
+var swiper = new Swiper(".mySwiper", {
+  spaceBetween: 30,
+  slidesPerView: 3,
+  centeredSlides: true,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+//   pagination: {
+//     el: ".swiper-pagination",
+//     // clickable: true,
+//   },
+
+  breakpoints: {
+    576: {
+      slidesPerView:4 ,
+      spaceBetween: 20,
+    },
+    768: {
+      slidesPerView: 6,
+      spaceBetween: 30,
+    }
+  },
+
+//   navigation: {
+//     nextEl: ".swiper-button-next",
+//     prevEl: ".swiper-button-prev",
+//   },
+});
